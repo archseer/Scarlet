da1ee8e1684f7533f2c38ccf59511d7e256ac845
Blaž Hrastnik <speed.the.bboy@gmail.com>
2012-04-21 02:17:33 +0200

    Oopsie...

--- commands/scarlet_commands.rb
+++ commands/scarlet_commands.rb
@@ -1,12 +1,16 @@
 Scarlet.hear (/what\'s playing\??/) do
   targ = target == $config.irc_bot.nick ? sender.nick : target
   if $bird
-    song = $bird.current_song
-    message = ["Now playing"]
-    message << (song.title ? "\"#{song.title}\"" : message << "\"#{File.basename(song.file)}\"")
-    message << "by \"#{song.artist}\"" if song.artist
-    message << "from the album \"#{song.album}\"" if song.album
-    msg targ, "#{message.join(' ')}."
+    if $bird.current_song
+      song = $bird.current_song
+      message = ["Now playing"]
+      message << (song.title ? "\"#{song.title}\"" : message << "\"#{File.basename(song.file)}\"")
+      message << "by \"#{song.artist}\"" if song.artist
+      message << "from the album \"#{song.album}\"" if song.album
+      msg targ, "#{message.join(' ')}."
+    else
+      msg targ, "No song playing."
+    end
   else
     msg targ, "Cardinal is not running at the moment."
   end
