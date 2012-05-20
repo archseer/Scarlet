8c32f1494888b5b17a7869948ed8cabefe1b276f
Blaž Hrastnik <speed.the.bboy@gmail.com>
2012-05-20 12:38:00 +0200

    Okay, I'm going to get murdered for this, but I de-() some stuff.

--- lib/info_table.rb
+++ lib/info_table.rb
@@ -4,19 +4,19 @@
 module Scarlet
   class ColumnTable
     VERSION = 0.0002
-    def self.mk_table(width,height,&block)
+    def self.mk_table width,height, &block
       Array.new(width) { Array.new(height,&block) }
     end
     attr_accessor :padding
-    def initialize(width,height)
+    def initialize width, height
       @cell_color= {}
       @col_color = {}
       @row_color = {}
       @padding = 2
-      resize!(width,height)
+      resize! width, height
     end
     attr_reader :width, :height
-    def resize!(width,height)
+    def resize! width, height
       @width, @height = width,height
       @data = ColumnTable.mk_table(@width,@height) { " " }
       self
@@ -25,53 +25,53 @@ module Scarlet
       @cell_color.clear
       @col_color.clear
       @row_color.clear
-      resize!(@width,@height)
+      resize! @width, @height
       self
     end
-    def get_cell(x,y)
+    def get_cell x, y
       return nil unless(x.between?(0,@width) && y.between?(0,@height))
       @data[x][y]
     end
-    def set_cell(x,y,value)
+    def set_cell x,y,value
       return unless(x.between?(0,@width) && y.between?(0,@height))
       @data[x][y] = value
       self
     end
-    def set_row(sx,y,*values)
+    def set_row sx,y,*values
       values.each_with_index do |str,x|
         set_cell(sx+x,y,str)
       end
       self
     end
-    def set_column(x,sy,*values)
+    def set_column x,sy,*values
       values.each_with_index do |str,y|
         set_cell(x,sy+y,str)
       end
       self
     end
     # // color_a = [int background,int text]
-    def cell_color(x,y)
+    def cell_color x, y
       @cell_color[[x,y]]
     end
-    def col_color(x)
+    def col_color x
       @col_color[x]
     end
-    def row_color(y)
+    def row_color y
       @row_color[y]
     end
-    def set_cell_color(x,y,*color_a)
+    def set_cell_color x, y, *color_a
       @cell_color[[x,y]] = color_a
       self
     end
-    def set_col_color(x,*color_a)
+    def set_col_color x, *color_a
       @col_color[x] = color_a
       self
     end
-    def set_row_color(y,*color_a)
+    def set_row_color y, *color_a
       @row_color[y] = color_a
       self
     end
-    def join_cells(*xys)
+    def join_cells *xys
       # // Uggggh
       self
     end
@@ -119,7 +119,7 @@ module Scarlet
   class InfoTable
     VERSION = 0.0002
     attr_accessor :colors, :width, :padding
-    def initialize(width=50)
+    def initialize width=50
       @width     = width
       @padding   = 0
       @colors    = {}
@@ -142,10 +142,10 @@ module Scarlet
       @lines.clear
       self
     end
-    def addHeader(string)
+    def addHeader string
       @headers << string
     end  
-    def addRow(string)
+    def addRow string
       @lines << string
       self
     end
