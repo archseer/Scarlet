8eb65e8811a4fd469fb96cd54dadc6fa1b6e861d
IceDragon200 <mistdragon100@gmail.com>
2012-05-19 19:26:09 -0500

    Hello array :3

--- commands/todo.rb
+++ commands/todo.rb
@@ -30,11 +30,9 @@ Scarlet.hear /show todo\s*(\d+)/ do
     table.set_row(0,1,"Date:"     ,crt     ).set_row_color(1,1,0)
     table.set_row(0,2,"By:"       ,t.by    ).set_row_color(2,1,0)
     table.set_row(0,3,"Entry:"    ,""      ).set_row_color(3,1,0)
-    wd, pad = table.column_widths, table.padding
+    wd, pad = table.column_widths.inject(:+), table.padding
     table.compile.each { |line| reply line, true }
-    msgs.each_with_index { |s,i| 
-      reply s.align(wd,:left,pad).irc_color(1,0)
-    }
+    msgs.each_with_index { |s,i| reply s.align(wd,:left,pad).irc_color(1,0) }
   else
     reply "TODO ##{id} could not be found."
   end
