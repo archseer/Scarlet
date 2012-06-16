29853d13b1c0d3598782bccad6f745ad1804225a
IceDragon200 <mistdragon100@gmail.com>
2012-06-16 17:14:57 -0500

    !win fix nick

--- commands/win.rb
+++ commands/win.rb
@@ -2,15 +2,15 @@
 Scarlet.hear /win(?:\s(\S+))?/i, :registered do
   given = !!params[1]
   nick = Scarlet::Nick.where(:nick=> params[1]).first
-  same = nick ? sender.nick.casecmp(nick.nick) : false
+  same = nick ? sender.nick.downcase == nick.nick.downcase : false
   if nick and !same 
     nick.win_points += 1
     nick.save!
     reply "#{sender.nick} gave #{params[1]} a win!" 
+  elsif given and !nick
+    reply "You can't win %s" % params[1]  
   elsif same 
     reply "You can't give yourself a win!" 
-  elsif given and !nick
-    reply "You can't win %s" % params[1]
   else
     wins = Scarlet::Nick.where(:nick=> sender.nick).first.win_points
     reply "#{sender.nick} has #{wins} #{"win point".pluralize(wins)}." 
