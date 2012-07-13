07129ba229bac6d51caf27800f4bd62b8419d490
IceDragon200 <mistdragon100@gmail.com>
2012-07-13 12:52:03 -0500

    recall fix, corrected help messages

--- commands/irc_basic_commands.rb
+++ commands/irc_basic_commands.rb
@@ -1,27 +1,27 @@
-# part <#channel>
+# part <#channel> - ask bot to leave #channel
 Scarlet.hear /(?:leave|part)\s(\#\S+)(?:\s(.+))?/i, :dev do
   send_data "PART #{params[1]} #{params[2]}"
 end
-# join <#channel>,<#channel>,<#channel>
+# join <#channel>[,<#channel>,<#channel>] - ask bot to join #channel or channels
 Scarlet.hear /join\s(.+)/i do
   params[1].gsub(' ','').split(',').each do |channel_name|
     channel_name = ?# + channel_name unless channel_name.start_with? ?#
     send_data "JOIN #{channel_name}"
   end
 end
-# send <string>
+# send <string> - send data to the server
 #Scarlet.hear /send\s(.+)/i, :dev do
 #  send_data params[1]
 #end
-# privmsg <#channel> <string>
+# privmsg <#channel|user> <string> - msg to #channel or user
 Scarlet.hear /privmsg\s(\S+)\s(.+)/i, :dev do
   send_data "PRIVMSG #{params[1]} #{params[2]}"
 end
-# notice <#channel> <string>
+# notice <#channel|user> <string> - send notice to @channel or user
 Scarlet.hear /notice\s(\S+)\s(.+)/i, :dev do
   send_data "NOTICE #{params[1]} #{params[2]}"
 end
-# cycle <#channel>
+# cycle <#channel> - leave and rejoin #channel 
 Scarlet.hear /cycle(?:\s(\S+))?/i, :dev do
   if params[1]
     send_data "CYCLE #{params[1]}"
--- commands/recall.rb
+++ commands/recall.rb
@@ -1,7 +1,7 @@
 # recall [<int>]- returns the last n messages, by default n is 5
 Scarlet.hear /recall(?:\s(\d+))?/ do
   if channel
-    recall_depth = ([[params[1],10].min,1].max || 5) + 1
+    recall_depth = ([[(params[1]||5).to_i,5].min,1].max) + 1
     logs = Scarlet::Log.channel(channel).privmsg.sort(:created_at.desc).limit(recall_depth).all.reverse 
     # because we use limit, that limits to the first n elements. 
     # if we want to get the last n, we need to reverse order, then limit then reverse again
