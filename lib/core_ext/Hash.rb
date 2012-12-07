9cce97c7bdbe6adb1cefb31d12e5185b79f6e22e
Blaž Hrastnik <speed.the.bboy@gmail.com>
2012-12-07 15:09:36 +0100

    Remove unless respond_to?, the reason the method got triggered is because it couldn't respond.

--- lib/core_ext/Hash.rb
+++ lib/core_ext/Hash.rb
@@ -25,18 +25,16 @@ class Hash
   # instead of hash[:key][:key], hash.key.key
   def method_missing(method, *args)
     method_name = method.to_s
-    unless respond_to? method_name
-      if method_name.ends_with? '?'
-        # if it ends with ? it's an existance check
-        method_name.slice!(-1)
-        key = keys.detect {|k| k.to_s == method_name }
-        return !!self[key]
-      elsif method_name.ends_with? '='
-        # if it ends with = it's a setter, so set the value
-        method_name.slice!(-1)
-        key = keys.detect {|k| k.to_s == method_name }
-        return self[key] = args.first
-      end
+    if method_name.ends_with? '?'
+      # if it ends with ? it's an existance check
+      method_name.slice!(-1)
+      key = keys.detect {|k| k.to_s == method_name }
+      return !!self[key]
+    elsif method_name.ends_with? '='
+      # if it ends with = it's a setter, so set the value
+      method_name.slice!(-1)
+      key = keys.detect {|k| k.to_s == method_name }
+      return self[key] = args.first
     end
     # if it contains that key, return the value
     key = keys.detect {|k| k.to_s == method_name }
