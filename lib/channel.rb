7c8b1eda86ef12a5c9f0495cd2b2f5d063e8f9a3
Blaž Hrastnik <speed.the.bboy@gmail.com>
2012-12-12 22:11:31 +0100

    User/Channel documentation.

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,8 +1,10 @@
 module Scarlet
+  # A representation of a channel on the network.
   class Channel
     attr_accessor :topic, :bans, :users, :modes, :user_flags
     attr_reader :name
 
+    # @param [String] name Name of the channel.
     def initialize name
       @name = name
       @users = Users.new
@@ -12,6 +14,7 @@ module Scarlet
       @topic = nil
     end
 
+    # @param [User] user Remove a user from the channel.
     def remove_user user
       @users.remove user.name
       @user_flags.delete user
@@ -22,19 +25,27 @@ module Scarlet
     end
   end
 
+  # Represents a collection of channels.
   class Channels
     def initialize
       @channels = {}
     end
 
+    # Gets the queried channel from the collection.
+    # @param [String] channel Name of the channel.
+    # @return [Channel, nil] The channel, or nil if no such channel exists.
     def get channel
       @channels[channel.to_s]
     end
 
+    # Adds a channel to the collection.
+    # @param [Channel] channel The channel we want to add to the collection.
     def add channel
       @channels[channel.name] = channel
     end
 
+    # Removes a channel from the list.
+    # @param [String] channel The channel of the channel we want to delete.
     def remove channel
       if channel.is_a? Channel
         @channels.delete(channel.name)
@@ -43,10 +54,13 @@ module Scarlet
       end
     end
 
+    # Clear the collection.
     def clear
       @channels.clear
     end
 
+    # Enumerate trough all channels on the list.
+    # @yield [Channel] Gives the channel to the block.
     def each(&block)
       @channels.values.each(&block)
     end
--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,10 @@
 module Scarlet
+  # A representation of a user on an IRC network.
   class User
     attr_accessor :name, :ns_login, :channels, :account_name
     alias :nick :name
 
+    # @param [String] name The name of the user.
     def initialize name
       @name = name
       @ns_login = false
@@ -14,28 +16,35 @@ module Scarlet
       "#<#{self.class.name}:#{self.object_id} name=#{@name}, ns_login=#{ns_login}>"
     end
 
+    # Add the user to a channel.
+    # @param [Channel] channel The channel we want to join.
     def join channel
       @channels.add channel
       channel.users.add self
     end
 
+    # Remove the user from a channel.
+    # @param [Channel] channel The channel we want to part.
     def part channel
       chan = @channels.remove(channel) # we can pass a string or Channel and it will return a Channel.
       chan.users.remove @name
     end
 
+    # Remove the user from all channels.
     def part_all
       @channels.each { |channel| channel.remove_user self }
       @channels.clear
     end
   end
 
+  # Represents a list we use as a storage for a collection of users.
   class Users
     def initialize
       @users = {}
     end
 
-    # gets an user if he exists, else it creates one.
+    # Gets an user if he exists, else it creates one.
+    # @param [String] user The user we are looking for.
     def get_ensured user
       if exist? user
         get user
@@ -44,35 +53,49 @@ module Scarlet
       end
     end
 
-    # checks if user exists
+    # Checks if user exists.
+    # @param [String] user The user we are looking for.
     def exist? user
       !!@users[user.to_s]
     end
 
+    # Returns the user we query for.
+    # @param [String] user The user we are looking for.
+    # @return [User, nil] The user, or nil if no such user exists.
     def get user
       @users[user.to_s]
     end
 
+    # Adds a user to the list.
+    # @param [User] user The user we are adding to the list.
     def add user
       @users[user.name] = user
     end
 
+    # Removes a user from the list.
+    # @param [String] user The name of the user we want to delete.
     def remove user
       @users.delete(user.to_s)
     end
 
+    # Removes the user from the list and parts the user from all channels.
+    # @param [String] user The name of the user that quit.
     def quit user
       remove(user).part_all
     end
 
+    # Clear the list.
     def clear
       @users.clear
     end
 
+    # Enumerate trough all users on the list.
+    # @yield [User] Gives the user to the block.
     def each(&block)
       @users.values.each(&block)
     end
 
+    # Returns the list of users as an array.
     def to_a
       @users.values
     end
