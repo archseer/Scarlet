b91b4d7f5d8909ffc9d0bed4d423147a94d15897
Corey Powell <mistdragon100@gmail.com>
2015-03-21 10:47:40 -0500

    Moved eval command to commands/eval.rb

--- commands/misc.rb
+++ commands/misc.rb
@@ -28,18 +28,3 @@ hear (/poke\s+(.+)/) do
     notice params[1], "#{sender.nick} has poked you."
   end
 end
-
-hear (/eval\s+(.+)/) do
-  clearance :dev
-  description 'Evals a provided string has ruby'
-  usage 'eval <string>'
-  on do
-    begin
-      t = Thread.new { Thread.current[:output] = "==> #{eval(params[1])}" }
-      t.join(10)
-      reply t[:output] if t[:output].size > 4
-    rescue Exception  => result
-      reply "ERROR: #{result.message}".irc_color(4,0)
-    end
-  end
-end
