115e73a045414b9bc9bc4c2918354390254a3029
Corey Powell <mistdragon100@gmail.com>
2015-03-21 10:48:02 -0500

    Updated google command to use helpers instead

--- commands/google.rb
+++ commands/google.rb
@@ -1,14 +1,17 @@
 require 'cgi'
-# google <terms> - Returns the first result on google matching <terms>.
+require 'scarlet/helpers/http_command_helper'
+require 'scarlet/helpers/json_command_helper'
+
 hear (/google\s+(.+)/) do
   clearance :any
   description 'Harness the power of google!'
-  usage 'google <your_search>'
+  usage 'google <terms>'
+  helpers Scarlet::HttpCommandHelper, Scarlet::JsonCommandHelper
   on do
-    http = EventMachine::HttpRequest.new('http://ajax.googleapis.com/ajax/services/search/web').get :query => {'v' => '1.0', 'q' => params[1]}
+    http = http_request('http://ajax.googleapis.com/ajax/services/search/web').get :query => {'v' => '1.0', 'q' => params[1]}
     http.errback { reply "ERROR! Fatal mistake." }
     http.callback do
-      results = JSON.parse(http.response)
+      results = json_parse http.response
       message = !results['responseData']['results'].empty? ? results['responseData']['results'][0]['url'] : "No search result found."
       reply message
     end
