0b1e0b09d1b48c5a27c8e769dbe54800d8d9e203
Corey Powell <mistdragon100@gmail.com>
2015-03-22 09:48:52 -0500

    Fixed seen command using old time-lord format

--- commands/seen.rb
+++ commands/seen.rb
@@ -1,15 +1,17 @@
+require 'time-lord'
+
 hear (/seen (?<nick>\S+)/i) do
   clearance :registered
   description 'When was the last time you saw nick?'
   usage 'seen <nick>'
   on do
-    log = Scarlet::Log.nick(params[:nick]).sort(:created_at.asc).last
+    log = Scarlet::Log.nick(params[:nick]).sort(:created_at.desc).first
     unless log
       reply "Sorry, I have never seen #{params[:nick]}."
       next
     end
 
-    message = "#{log.nick} was last seen #{log.created_at.ago_in_words}"
+    message = "#{log.nick} was last seen #{log.created_at.ago.to_words}"
     case log.command.downcase.to_sym
     when :privmsg
       message << " in #{log.target}" unless log.target.downcase == channel
