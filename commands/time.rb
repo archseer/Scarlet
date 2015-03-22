23d4b22ed7c3c69cfa62846052d2cf16388c19d0
Corey Powell <mistdragon100@gmail.com>
2015-03-22 10:10:24 -0500

    unless else, really?
    Also fixed the response reporting the old command format

--- commands/time.rb
+++ commands/time.rb
@@ -18,24 +18,25 @@ hear (/set(?:\s+my)?\s+timezone\s+(?<timezone>.+)/i) do
   end
 end
 
-hear (/time(?:\s(\S+))?/i) do
+hear (/time(?:\s+(?<nick>\S+))?/i) do
   clearance :registered
   description 'Prints the current owners time.'
   usage 'time'
   on do
-    unless params[1]
-      reply Time.now
-    else
+    nick = params[:nick]
+    if nick
       nck = params[1].gsub(/-me/i, sender.nick)
       if nick = Scarlet::Nick.first(:nick => nck)
         if zone_str = nick.settings[:timezone]
           reply Time.now.in_time_zone(zone_str)
         else
-          reply "Your timezone is not set: Use !settings timezone your_timezone_string"
+          reply 'Your timezone is not set: Use !set timezone <your_timezone_string>'
         end
       else
         reply 'Cannot view time for "%s".' % params[1]
       end
+    else
+      reply Time.now
     end
   end
 end
