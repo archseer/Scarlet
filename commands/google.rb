b8339d43dde4dacbe843e9339f9cc6269a4bf166
Corey Powell <mistdragon100@gmail.com>
2015-03-22 09:48:27 -0500

    Fixed typo in google command

--- commands/google.rb
+++ commands/google.rb
@@ -11,7 +11,7 @@ hear (/google\s+(.+)/) do
     http = http_request('http://ajax.googleapis.com/ajax/services/search/web').get :query => {'v' => '1.0', 'q' => params[1]}
     http.errback { reply "ERROR! Fatal mistake." }
     http.callback do
-      results = json_parse http.response
+      results = parse_json http.response
       message = !results['responseData']['results'].empty? ? results['responseData']['results'][0]['url'] : "No search result found."
       reply message
     end
