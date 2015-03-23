8ad5d6d01ae86184b84affb2b08f06d53fadd144
Corey Powell <mistdragon100@gmail.com>
2015-03-22 19:11:21 -0500

    Added pong command

--- commands/ping.rb
+++ commands/ping.rb
@@ -6,3 +6,12 @@ hear (/ping/i) do
     reply 'PONG!'
   end
 end
+
+hear (/pong/i) do
+  clearance :any
+  description 'Pongs the bot, should respond with PING!'
+  usage 'pong'
+  on do
+    reply 'PING!'
+  end
+end
