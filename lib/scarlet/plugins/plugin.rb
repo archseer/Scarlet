a2e70be253e18223cf24d547eb3e084619c2d8d5
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-05-02 11:28:20 +0900

    Plugin blueprints

--- lib/scarlet/plugins/plugin.rb
+++ lib/scarlet/plugins/plugin.rb
@@ -1,10 +1,42 @@
 module Scarlet
   module Plugin
-    def join listeners
-      plugin = self
-      listeners.on event_name do |event|
-        plugin.invoke event
-      end
+    # define @@listeners = Listeners.new in the included module
+
+    def on(command, *args, &block)
+      @@listeners.on(command, *args, &block)
     end
   end
 end
+
+module Scarlet::Plugin
+  module Autojoin
+    include Scarlet::Plugin
+    def print_uri m
+      puts m
+    end
+
+    on :privmsg do
+      url = event.msg.match(URL_REGEX)
+      print_uri url if url
+    end
+  end
+end
+
+Scarlet.config do
+  use Scarlet::Autojoin
+end
+
+use calls Scarlet::Handlers.use
+def use plug
+  @plugins << plug.new
+end
+
+def Scarlet::Handlers.trigger ev
+  trigger_own_handles
+  @plugins.each do |plug|
+    plug.trigger ev
+  end
+end
+
+# later on, event handles get evald inside a State hash object or POD State object,
+# which is serializable to disk.
