d84b5ab7869e37187dda365e6a61bb871a4a9bdd
Corey Powell <mistdragon100@gmail.com>
2015-05-03 20:26:33 -0500

    Move checks to an instance method.

--- lib/scarlet/plugins/autoname.rb
+++ lib/scarlet/plugins/autoname.rb
@@ -4,20 +4,22 @@ module Scarlet::Plugins
   class Autoname
     include Scarlet::Plugin
 
-    on :quit do |event|
+    def update_nick?(event)
       my_nick = event.server.config.nick
       current_nick = event.server.current_nick
       sender_nick = event.sender.nick
       # if the my current nick is equal to the one from the config
-      unless current_nick == my_nick
-        # if we're quitting we can just skip as well
-        unless sender_nick == current_nick
-          # ignore if the quitter isn't who we want
-          unless sender_nick != my_nick
-            event.send "NICK #{my_nick}"
-          end
-        end
-      end
+      return false if current_nick == my_nick
+      # if we're quitting we can just skip as well
+      return false if sender_nick == current_nick
+      # ignore if the quitter isn't who we want
+      return false if sender_nick != my_nick
+      true
+    end
+
+    on :quit do |event|
+      my_nick = event.server.config.nick
+      event.send "NICK #{my_nick}" if update_nick?(event)
     end
   end
 end
