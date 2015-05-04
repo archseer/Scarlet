5cbb3257acf97cc4ca4727ed95f76a730ceb5ec2
Corey Powell <mistdragon100@gmail.com>
2015-05-03 20:14:37 -0500

    Fixed "unexpected return" in autoname plugin

--- lib/scarlet/plugins/autoname.rb
+++ lib/scarlet/plugins/autoname.rb
@@ -9,13 +9,15 @@ module Scarlet::Plugins
       current_nick = event.server.current_nick
       sender_nick = event.sender.nick
       # if the my current nick is equal to the one from the config
-      return if current_nick == my_nick
-      # if we're quitting we can just skip as well
-      return if sender_nick == current_nick
-      # ignore if the quitter isn't who we want
-      return if sender_nick != my_nick
-
-      event.send "NICK #{my_nick}"
+      unless current_nick == my_nick
+        # if we're quitting we can just skip as well
+        unless sender_nick == current_nick
+          # ignore if the quitter isn't who we want
+          unless sender_nick != my_nick
+            event.send "NICK #{my_nick}"
+          end
+        end
+      end
     end
   end
 end
