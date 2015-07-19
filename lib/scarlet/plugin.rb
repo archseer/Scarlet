3dbef28b856b711ce3749b4b614653cd472c33d3
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-07-19 12:36:49 +0100

    Stop delegating to event.server now

--- lib/scarlet/plugin.rb
+++ lib/scarlet/plugin.rb
@@ -68,7 +68,7 @@ class Scarlet
       klass = self.class
       execute = lambda do |block|
         begin
-          cxt = Scarlet::Context.new(event.dup, klass.helpers, self, event.server)
+          cxt = Scarlet::Context.new(event.dup, klass.helpers, self)
           cxt.exec(&block)
         rescue Exception => ex
           logger.error ex.inspect
