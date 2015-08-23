8782525030118d4beefa1d5498c0522b50861a8b
Corey Powell <mistdragon100@gmail.com>
2015-08-23 17:36:16 -0500

    Added a place holder party command
    Closes #34
    
    Because for some reason I can't remember the original text!
    
    https://www.youtube.com/watch?v=Zetcb-ny0Fg

---
+++ commands/party.rb
@@ -0,0 +1,10 @@
+# This is a very important command, in times of celebration this command is
+# required.
+hear(/party/) do
+  clearance nil
+  description 'Do you really need a description?'
+  usage 'party'
+  on do
+    reply "Celebrate good times, come on, let's celebrate!"
+  end
+end
