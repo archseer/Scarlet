71db5abe487b08c18d52d6c4ad29387ed7bfd82f
Corey Powell <mistdragon100@gmail.com>
2015-08-31 11:40:24 -0500

    Added ExpirarionCache
    This can store entries for a limited amount of time, after the set
    has expired, the data is removed from the cache.

---
+++ lib/scarlet/expiration_cache.rb
@@ -0,0 +1,68 @@
+require 'time-lord'
+require 'thread'
+require 'scarlet/logger'
+
+class Scarlet
+  class ExpirationCache
+    include Scarlet::Loggable
+
+    def initialize
+      @mutex = Mutex.new
+      @entries = {}
+      schedule_expiration
+    end
+
+    def new_expiration
+      5.minutes.from_now
+    end
+
+    def schedule_time
+      10.minutes
+    end
+
+    def modify
+      @mutex.synchronize do
+        yield
+      end
+    end
+
+    private def reset_expiration(entry)
+      entry[:expiration] = new_expiration
+    end
+
+    def schedule_expiration
+      EM.add_timer schedule_time do
+        logger.info 'Expiring old cache entries'
+        modify do
+          @entries.reject! do |key, data|
+            Time.now >= data[:expiration]
+          end
+        end
+        schedule_expiration
+      end
+    end
+
+    def [](key)
+      logger.info "Accessing cache: key=#{key}"
+      data = nil
+      modify do
+        data = @entries[key]
+        reset_expiration(data) if data
+      end
+      return data && data[:value]
+    end
+
+    def []=(key, value)
+      logger.info "Modifying cache: key=#{key}"
+      modify do
+        data = @entries[key] ||= {}
+        data[:value] = value
+        reset_expiration(data)
+      end
+    end
+
+    def self.instance
+      @instance ||= new
+    end
+  end
+end
