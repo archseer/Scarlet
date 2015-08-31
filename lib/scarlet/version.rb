03e081215af69272ae1fb1720379a4a543d3356c
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-09-01 01:08:12 +0200

    Bump the version!
    Because why the heck not.

--- lib/scarlet/version.rb
+++ lib/scarlet/version.rb
@@ -1,6 +1,6 @@
 class Scarlet
   module Version
-    MAJOR, MINOR, TEENY, PATCH = 1, 3, 0, nil
+    MAJOR, MINOR, TEENY, PATCH = 1, 7, 0, nil
     STRING = [MAJOR, MINOR, TEENY, PATCH].compact.join('.').freeze
   end
   VERSION = Version::STRING
