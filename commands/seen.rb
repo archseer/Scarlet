b2f0f11fe7b3c85162555d4a0a2e8801ac8f7060
Corey Powell <mistdragon100@gmail.com>
2015-10-29 11:37:12 -0500

    seen command now supports last N messages
    Example
    	@seen IceDragon last 5

--- commands/seen.rb
+++ commands/seen.rb
@@ -1,36 +1,39 @@
 require 'time-lord'
 
-hear(/seen (?<nick>\S+)/i) do
+hear(/seen (?<nick>\S+)(?:\s+last\s+(?<backlog>\d+))?/i) do
   clearance(&:registered?)
   description 'When was the last time you saw nick?'
-  usage 'seen <nick>'
+  usage 'seen <nick> [last <count>]'
   on do
-    log = server.logs.nick(params[:nick]).sort_by(&:updated_at).last
-    unless log
-      reply "Sorry, I have never seen #{params[:nick]}."
-      next
+    n = [1, [(params[:backlog] || 1).to_i, 10].min].max
+    logs = server.logs.nick(params[:nick]).sort_by(&:updated_at).last(n)
+    if logs.blank?
+      error_reply "Sorry, I have never seen #{params[:nick]}."
     end
 
-    message = "#{log.nick} was last seen #{Time.at(log.updated_at).ago.to_words}"
-    case log.command.downcase.to_sym
-    when :privmsg
-      message << " in #{log.target}" unless log.target.downcase == event.channel
-      if log.message =~ /\u0001ACTION (.+)\u0001/
-        message << " doing '/me #{$1}'."
+    reply "#{params[:nick]} was last seen:"
+    logs.each do |log|
+      message = "#{Time.at(log.updated_at).ago.to_words}"
+      case log.command.downcase.to_sym
+      when :privmsg
+        message << " in #{log.target}" unless log.target.downcase == event.channel
+        if log.message =~ /\u0001ACTION (.+)\u0001/
+          message << " doing '/me #{$1}'."
+        else
+          message << " saying '#{log.message}'."
+        end
+      when :nick
+        message << " changing nickname to '#{log.target}'."
+      when :join
+        message << " joining channel #{log.target}."
+      when :part
+        message << " leaving channel #{log.target}."
+      when :quit
+        message << " quiting IRC."
       else
-        message << " saying '#{log.message}'."
+        message = "Not yet implemented!"
       end
-    when :nick
-      message << " changing nickname to '#{log.target}'."
-    when :join
-      message << " joining channel #{log.target}."
-    when :part
-      message << " leaving channel #{log.target}."
-    when :quit
-      message << " quiting IRC."
-    else
-      message = "Not yet implemented!"
+      reply message
     end
-    reply message if message
   end
 end
