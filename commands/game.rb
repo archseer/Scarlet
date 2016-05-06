85623f81fa7f856701266178febeb2914a8438a7
Corey Powell <mistdragon100@gmail.com>
2016-05-06 10:33:20 -0500

    game commands now use a config file to control the die and coin limits

--- commands/game.rb
+++ commands/game.rb
@@ -1,22 +1,35 @@
-﻿hear(/dice\s+(\d+)d(\d+)/i) do
+﻿config = load_config_file do
+  {
+    dies: {
+      sides_limit: 24,
+      dies_limit: 12
+    },
+    coins: {
+      limit: 24
+    }
+  }
+end
+
+hear(/dice\s+(?<dies>\d+)d(?<sides>\d+)/i) do
   clearance nil
   description 'rolls <x> dies with <y> sides.'
   usage 'dice <x>d<y>'
   on do
-    sides, diecoun = params[2].to_i, params[1].to_i
-    sides   = 1 if sides <= 0
-    diecoun = 1 if diecoun <= 0
-    dice    = diecoun.times.collect{|i| 1 + rand(sides)}
-    reply format("%d : %s", dice.inject(0){|r,i|r+i}, dice.inspect)
+    dies  = params[:dies].to_i.minmax(1, config.dig(:dies, :dies_limit))
+    sides = params[:sides].to_i.minmax(4, config.dig(:dies, :sides_limit))
+    sides = 1 if sides <= 0
+    dies  = 1 if dies <= 0
+    dice  = dies.times.collect { |i| 1 + rand(sides) }
+    reply format("%d : %s", dice.inject(:+), dice.inspect)
   end
 end
 
-hear(/coin\s+(\d+)/i) do
+hear(/coin\s+(?<coins>\d+)/i) do
   clearance nil
   description 'Flips <x> coins O is heads, and X is tails.'
   usage 'coin <x>'
   on do
-    count, = params[1].to_i
+    count = params[:coins].to_i.minmax(1, config.dig(:coins, :limit))
     reply count.times.map{ |i| rand(2) == 0 ? 'O' : 'X' }.inspect.gsub('"',"")
   end
 end
