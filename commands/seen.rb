b372886b168ed518a24afe9ef4207b547b0c6eeb
Corey Powell <mistdragon100@gmail.com>
2016-05-06 10:34:56 -0500

    Prevent seen command from returning information on a foreign channel
    Security Reasons.

--- commands/seen.rb
+++ commands/seen.rb
@@ -6,7 +6,12 @@ hear(/seen (?<nick>\S+)(?:\s+last\s+(?<backlog>\d+))?/i) do
   usage 'seen <nick> [last <count>]'
   on do
     n = [1, [(params[:backlog] || 1).to_i, 10].min].max
-    logs = server.logs.nick(params[:nick]).sort_by(&:updated_at).last(n)
+    logs = server.logs.
+      channel(event.channel).
+      select { |log| log.nick == params[:nick] }.
+      sort_by(&:updated_at).
+      last(n)
+
     if logs.blank?
       error_reply "Sorry, I have never seen #{params[:nick]}."
     end
